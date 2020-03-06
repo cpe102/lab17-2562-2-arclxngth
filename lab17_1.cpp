041dfa5ec0f367dc572d6febcbceb3cfdb8633f5
arclxngth <59277471+arclxngth@users.noreply.github.com>
2020-03-06 17:15:36 +0700

    ver 1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -8,27 +8,85 @@ using namespace std;
 int main(){
 
     vector <string> name_list;
-    vector <string> score_list;
+    vector <int> score_list;
+    vector <string> grade_list;
     string input;
 
-    string format;
+    char name[100];
+    int num1, num2, num3;
 
-    ifstream text("name_score.txt");
+    ifstream text("C:\\Users\\acer\\Documents\\GitHub\\lab17-2562-2-arclxngth\\name_score.txt");
 
     while(getline(text,input)){
 
-        sscanf(input,"%s:%s %d",name_list[0]);
-        cout << name_list;
+        int sum=0;
+
+        sscanf(input.c_str(),"%[^:]:%d %d %d",name,&num1,&num2,&num3);
+
+        sum = num1+num2+num3;
+
+        name_list.push_back(name);
+        score_list.push_back(sum);
+
     }
 
-    // string command;
+    for(int i=0; i<score_list.size(); i++){
 
-    // cout<<"Please input your command : ";
-    // getline(cin,command);
+        if(score_list[i]>=80)         grade_list.push_back("a");
+        else if(score_list[i]>=70)    grade_list.push_back("b");
+        else if(score_list[i]>=60)    grade_list.push_back("c");
+        else if(score_list[i]>=50)    grade_list.push_back("d");
+        else                          grade_list.push_back("e");
+    }
 
-    // if(command)
+    while(true){
 
-    // if(command)
+        string command;
+        int i=0;
 
-    // if(command == "exit")   break;
+        cout<<"Please input your command : ";
+        getline(cin,command);
+        
+        while(command[i]){
+
+            command[i] = tolower(command[i]);
+            i++;
+        }
+        
+        int space_pos = command.find_first_of(" ");
+        string command_main = command.substr(0,space_pos);
+
+        string target = command.substr(space_pos,command.size());
+
+        int count = 0;
+
+        if(string(command_main) == "name"){
+
+            for(int i=0; i<name_list.size(); i++){
+
+                if(name_list[i] == target)  {
+
+                    cout<<name_list[i]<<"'s grade = "<<grade_list[i];
+                    count++;
+                }
+            }
+            if(count = 0) cout<<"Cannot found";
+        }
+
+        else if(string(command_main) == "grade"){
+
+            for(int i=0; i<grade_list.size(); i++){
+
+                if(grade_list[i] == target)  {
+                    
+                    cout<<name_list[i];
+                    count++;
+            }
+        }
+
+            if(count=0) cout<<"Cannot found";
+        }
+
+        else    cout<<"Invalid command";
+    }
 }
