158756186746457ce50de0263c4e1bc67b76d509
arclxngth <59277471+arclxngth@users.noreply.github.com>
2020-03-06 16:58:51 +0700

    version 1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -1,1 +1,34 @@
-//Write your code here
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector <string> name_list;
+    vector <string> score_list;
+    string input;
+
+    string format;
+
+    ifstream text("name_score.txt");
+
+    while(getline(text,input)){
+
+        sscanf(input,"%s:%s %d",name_list[0]);
+        cout << name_list;
+    }
+
+    // string command;
+
+    // cout<<"Please input your command : ";
+    // getline(cin,command);
+
+    // if(command)
+
+    // if(command)
+
+    // if(command == "exit")   break;
+}
